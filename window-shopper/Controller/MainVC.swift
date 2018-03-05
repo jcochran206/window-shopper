c16e4927cd1687655bba986c2b94651e96c96018
jcochran206 <jonathan.o.cochran@gmail.com>
2018-03-04 22:17:43 -0800

    finished UI frame and added calcBtn to animate with calculator

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -15,9 +15,20 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let calcBtn = UIButton(frame: CGRect(x:0, y: 0, width: view.frame.size.width, height: 60))
-        calcBtn.backgroundColor = UIColor.orange
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
         
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+    
+    
+    @objc func calculate(){
+        print("we got here")
     }
 
     override func didReceiveMemoryWarning() {
